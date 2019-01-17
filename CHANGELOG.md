f9184696b774ba119239b6413f049e8bb598d657
Brandon Roberts <brobertstech@gmail.com>
2019-01-16 19:16:07 -0800

    Add changelog and add api section in the readme

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 1.0.1
+- :hammer: Remove code for calculating total payment of the life of a loan (currently not used or returned)
+
+# 1.0.0 :rocket:
+- Initial release
+- Added a function to calculate how much interest would be gained on an investment.
+- Added function to calculate how much interest would be payed over the life of a loan.
--- src/loan-interest.js
+++ src/loan-interest.js
@@ -4,19 +4,13 @@
 const loanInterest = (principal, interestRate, monthlyPayment) => {
   let principalLeft = principal
   let totalInterest = 0
-  let totalPayment = 0
-  let numMonths = 0
 
   while (principalLeft >= 0) {
     const monthInterest = principalLeft * interestRate * (1 / 12)
     const appliedToPrincipal = monthlyPayment - monthInterest
     const nextPrincipal = principalLeft - appliedToPrincipal
-
-    totalPayment +=
-      nextPrincipal < 0 ? principalLeft + monthInterest : monthlyPayment
     totalInterest += monthInterest
     principalLeft = nextPrincipal
-    numMonths++
   }
   return totalInterest
 }
